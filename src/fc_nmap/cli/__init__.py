fd659dc3d99bac621de079398523f97105da2ad2
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2024-10-06 20:19:20 +0300

    v0.0.1
    `fc-nmap scan` will scan the network and generate a list of hubs.

--- src/fc_nmap/cli/__init__.py
+++ src/fc_nmap/cli/__init__.py
@@ -2,11 +2,42 @@
 #
 # SPDX-License-Identifier: MIT
 import click
+from datetime import datetime
+import random
 
 from fc_nmap.__about__ import __version__
-
+from fc_nmap.get_hubs import get_hubs
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="fc-nmap")
 def fc_nmap():
-    click.echo("Hello world!")
+    pass
+
+@fc_nmap.command()
+@click.option('--hub', default='', help='IP:port of hub to start crawling from.')
+@click.option('--hops', default=20, help='Number of hubs to query.')
+@click.option('--out', default='-', type=click.File('w'), help="Output file, leave empty for stdout")
+def scan(hub, hops, out):
+    """fc-nmap scan will scann the network starting from [HUB] and collect hub IPs, versions and last
+    update timestamp.
+    """
+    hubs = {}
+    with click.progressbar(
+            length=hops, 
+            label='Scanning', 
+            width=0,
+            show_eta=False,
+            bar_template='%(label)s  %(bar)s  %(info)s',
+            #fill_char='█',
+            fill_char='#',
+            item_show_func=lambda a: a.rjust(21) if a else None
+        ) as bar:
+        for i in range(hops):
+            bar.update(1, hub)
+            hubs = get_hubs(hub, hubs)
+            hub = random.choice(list(hubs.keys()))
+        bar.update(1,'Done.')
+    for h in hubs:
+        click.echo(f'{h}\t{hubs[h]['appv']}\t{datetime.fromtimestamp(int(hubs[h]['timestamp']/1000), tz=None)}', file=out)
+    
+    click.echo(f'Total hubs: {len(hubs)}')
---
+++ src/fc_nmap/get_hubs.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from farcaster.HubService import HubService
+import time, datetime, random
+
+def get_hubs(hub_address, hubs):
+	if not hub_address:
+		print("No hub address. Check .env.sample")
+		sys.exit(1)
+	peers = None
+	while not peers:
+		try:
+			hub = HubService(hub_address, use_async=False)
+			peers = hub.GetCurrentPeers()
+		except:
+			# print(f"=== {hub_address} did not respond")
+			# print(f'=== {hub_address}\t{hubs[hub_address]['appv']}\t{datetime.datetime.fromtimestamp(int(hubs[hub_address]['timestamp']/1000), tz=None)}')
+			hub_address = random.choice(list(hubs.keys()))
+			time.sleep(1)
+
+	
+	for c in peers.contacts:
+		if c.rpc_address.address != '127.0.0.1':
+			# print(c)
+			id = f'{c.rpc_address.address}:{c.rpc_address.port}'
+			if id not in hubs:
+				# print(f'n {id}\t{c.app_version}\t\t{datetime.datetime.fromtimestamp(int(c.timestamp/1000), tz=None)}')
+				hubs[id] = {
+					'hubv': c.hub_version,
+					'appv': c.app_version,
+					'timestamp': c.timestamp
+					}
+
+			if id in hubs and hubs[id]['timestamp'] < c.timestamp:
+				# if hubs[id]['appv'] != c.app_version:
+				#	print(f'u {id}\t{c.app_version}({hubs[id]['appv']})\t{datetime.datetime.fromtimestamp(int(c.timestamp/1000), tz=None)}')
+				#else:
+				#	print(f't {id}\t{c.app_version}({hubs[id]['appv']})\t{datetime.datetime.fromtimestamp(int(c.timestamp/1000), tz=None)}\t{datetime.datetime.fromtimestamp(int(hubs[id]['timestamp']/1000), tz=None)}')
+
+				hubs[id] = {
+					'hubv': c.hub_version,
+					'appv': c.app_version,
+					'timestamp': c.timestamp
+					}
+			#	print(f't {id}\t{c.app_version}\t\t{datetime.datetime.fromtimestamp(int(c.timestamp/1000), tz=None)}')
+
+			
+	# print('=== Last Hub queried: ', hub_address )
+	# print('=== Total hubs: ', len(hubs) )
+	
+	# print('=== Next Hub to be queried: ', hub_address )
+	# print()
+	hub.close()
+	return hubs
+
+#print(hubs)
